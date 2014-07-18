0108d14bbcff7b3465cf1557de4dc7a8439c97be
Goffin Simon <simon.goffin@outlook.com>
2014-07-18 09:03:21 +0200

    Add an inheritance mechanism(corrigé).

--- models/partner.py
+++ models/partner.py
@@ -2,11 +2,16 @@
 from openerp.osv import osv, fields 
 
 class Partner(osv.Model):
+    
+    """Inherited res.partner"""
 
 
     _inherit = 'res.partner'
     _columns = {
         'instructor' : fields.boolean("Instructor"),
+        
+        # Relational fields
+        'session_ids': fields.many2many("openacademy.session", string="Session"),
     }
     _defaults = {
         'instructor' : False,
--- models/session.py
+++ models/session.py
@@ -8,7 +8,7 @@ class Session(osv.Model):
         'start_date' : fields.date(string="Start date"),
         'duration' : fields.float(string="Duration", digits=(6,2),help="Duration in days"),
         'seats' : fields.integer(string="Number of seats"),
-        'instructor_id' : fields.many2one('res.partner', string="Instructor"),
+        'instructor_id' : fields.many2one('res.partner', string="Instructor",domain=[('instructor','=',True)]),
         'course_id' : fields.many2one('openacademy.course',
             ondelete='cascade', string="Course", required=True),
         'attendee_ids' : fields.one2many('openacademy.attendee', 'session_id',
