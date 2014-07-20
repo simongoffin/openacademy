41c4ac6c2a5ba34db47a511d556cd01ea2f5aae6
Goffin Simon <simon.goffin@outlook.com>
2014-07-20 21:51:30 +0200

    Create methods(wizard).

--- wizard/create_attendee.py
+++ wizard/create_attendee.py
@@ -2,6 +2,15 @@ from openerp.osv import fields, osv
 
 class CreateAttendeeWizard(osv.TransientModel):
     _name = 'openacademy.create.attendee.wizard'
+    
+    def action_add_attendee(self, cr, uid, ids, context=None):
+        session_model = self.pool.get('openacademy.session')
+        wizard = self.browse(cr, uid, ids[0], context=context)
+        session_ids = [sess.id for sess in wizard.session_ids]
+        att_data = [{'partner_id':att.partner_id.id} for att in wizard.attendee_ids]
+        session_model.write(cr, uid, session_ids,
+            {'attendee_ids': [(0, 0, data) for data in att_data]}, context)
+        return {}
 
     _columns = {
         'session_id': fields.many2one('openacademy.session', 'Session',required=True),
